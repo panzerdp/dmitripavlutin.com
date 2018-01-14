02d9fd45fa3ada743e455595728bb251d09b5064
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-01-14 15:10:13 +0200

    Add normalize.css

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import 'normalize.css/normalize.css';
 import styles from './index.module.scss';
+
+
 
 class Template extends React.Component {
   render() {
-    console.log(styles);
     const { children } = this.props;
     return (
       <div className={styles.container}>
