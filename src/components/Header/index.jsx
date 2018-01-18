1bb9ff279ca6a3e0c470864a57ddf2680ce7273c
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-01-18 17:57:28 +0200

    Header content and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,16 +6,18 @@ import styles from './index.module.scss';
 export default function Header() {
   return (
     <header>
-      <Link to="/">Home</Link>
-      <div className={styles.autor}>
-        <div className={styles.specialist}>
+      <div className={styles.headerContent}>
+        <Link to="/">
           <img
             className={styles.picture}
             src="https://www.gravatar.com/avatar/0d57a57d8807ebc70e24b46f6d9e3a36?s=250&d=mm&r=x"
           />
-          <p className={styles.software}>Dmitri Pavlutin</p>
-          <p className={styles.speciality}>JavaScript and React</p>
-        </div></div>
+        </Link>
+        <div>
+          <Link to="/" className={styles.name}>Dmitri Pavlutin</Link>
+          <div className={styles.speciality}>Likes JavaScript &amp; React</div>
+        </div>
+      </div>
     </header>
   );
 }
