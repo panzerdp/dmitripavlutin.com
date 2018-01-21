66bf4b3e5daae53b5eaee8996b877a0a15920b49
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-01-21 13:27:29 +0200

    prismjs integration

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-
 import 'normalize.css/normalize.css';
+
 import styles from './index.module.scss';
 
 import Header from '../components/Header';
--- src/templates/BlogPost/index.jsx
+++ src/templates/BlogPost/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import get from 'lodash/get';
+import 'prismjs/themes/prism-okaidia.css';
 
 import styles from './index.module.scss';
 
@@ -17,32 +18,6 @@ class BlogPostTemplate extends React.Component {
         <h1>{post.frontmatter.title}</h1>
         <p>{post.frontmatter.date}</p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr style={{}} />
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          {previous && (
-            <li>
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            </li>
-          )}
-
-          {next && (
-            <li>
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            </li>
-          )}
-        </ul>
       </article>
     );
   }
