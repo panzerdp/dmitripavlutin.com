545999802bac226a99cd8d703233db7fdaada0a0
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-03-18 17:37:09 +0200

    Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import styles from './index.module.scss';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className={styles.footerContent}>
+        Footer content
+      </div>
+    </footer>
+  );
 }
