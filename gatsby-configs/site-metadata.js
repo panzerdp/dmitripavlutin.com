72b475eecf9a9fd65dcb603f964d93877fcd6097
Dmitri Pavlutin <dpavlutin@gmail.com>
2018-03-24 16:00:51 +0200

    Site metadata to a separated file

---
+++ gatsby-configs/site-metadata.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Dmitri Pavlutin: JavaScript and React thoughts',
+  author: 'Dmitri Pavlutin',
+  description: 'Posts by Dmitri Pavlutin about JavaScript and React',
+  speciality: 'JavaScript & React developer',
+  siteUrl: 'https://dmitripavlutin.com',
+  postsPerPage: 5,
+  profiles: {
+    stackoverflow: 'https://stackoverflow.com/users/1894471/dmitri-pavlutin',
+    twitter: 'https://twitter.com/panzerdp',
+    linkedin: 'https://www.linkedin.com/in/dmitri-pavlutin/',
+    github: 'https://github.com/panzerdp',
+    facebook: 'https://www.facebook.com/dmitri.pavlutin'
+  },
+  nicknames: {
+    twitter: '@panzerdp'
+  }
+};
