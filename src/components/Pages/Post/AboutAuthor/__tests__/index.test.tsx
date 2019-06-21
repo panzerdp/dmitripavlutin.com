225fad77df49b8572191c47eb1ad8902e555ca03
Dmitri Pavlutin <dpavlutin@gmail.com>
2019-06-21 15:33:56 +0300

    About author unit tests

---
+++ src/components/Pages/Post/AboutAuthor/__tests__/index.test.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import PostAboutAuthor from '../index';
+
+const props = {
+  authorInfo: {
+    name: 'Dmitri Pavlutin',
+    nicknames: {
+      twitter: 'panzerdp',
+    },
+    profiles: {
+      facebook: 'https://www.facebook.com/dmitri.pavlutin',
+      github: 'https://github.com/panzerdp',
+      linkedin: 'https://www.linkedin.com/in/dmitri-pavlutin/',
+      stackoverflow: 'https://stackoverflow.com/users/1894471/dmitri-pavlutin',
+      twitter: 'https://twitter.com/panzerdp',
+    },
+    description: 'Dmitri Pavlutin is a software developer specialized in Frontend technologies',
+  },
+  authorProfilePictureSrc: '/image.png',
+};
+
+describe('<PostAboutAuthor />', function() {
+  it('should render author profile picture', function() {
+    const wrapper = shallow(<PostAboutAuthor {...props} />);
+    expect(wrapper.find(`img[src="${props.authorProfilePictureSrc}"]`)).toHaveLength(1);
+  });
+
+  it('should render author description', function() {
+    const wrapper = shallow(<PostAboutAuthor {...props} />);
+    expect(wrapper.text()).toContain(props.authorInfo.description);
+  });
+});
