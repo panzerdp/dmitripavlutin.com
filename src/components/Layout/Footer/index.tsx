5ad52da16939faf15fd4fb6015196823e7243683
Dmitri Pavlutin <dpavlutin@gmail.com>
2019-07-18 17:03:50 +0300

    Fix RSS URL

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -25,8 +25,8 @@ export default function LayoutFooter({ authorInfo: { profiles, name, email } }: 
         <div className={styles.links}>
           <div className={styles.footerNavigation}>
             <Link to={TO_INDEX()}>Home</Link>
-            <a href={TO_NEWSLETTER()}>Newsletter</a>
-            <Link to={TO_RSS()}>RSS</Link>
+            <Link to={TO_NEWSLETTER()}>Newsletter</Link>
+            <a href={TO_RSS()}>RSS</a>
             <Link to={TO_ALL_POSTS()}>All posts</Link>
             <Link to={TO_ABOUT_ME()}>About me</Link>
           </div>
