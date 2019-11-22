a71f411db775983f809b8cdc88f5dffe21ec761f
Dmitri Pavlutin <dpavlutin@gmail.com>
2019-11-22 11:53:25 +0200

    Increase netlify build speed

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,5 +99,6 @@ module.exports = {
     require('./gatsby/config/gatsby-plugin-sass'),
     'gatsby-plugin-minify-classnames',
     require('./gatsby/config/gatsby-plugin-feed'),
+    'gatsby-plugin-netlify-cache',
   ],
 };
