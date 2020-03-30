0b982c21ff557625acfab2bc44a0a86a016182c4
Dmitri Pavlutin <dpavlutin@gmail.com>
2020-03-30 16:41:29 +0300

    Coach widget

--- src/components/AboutAuthor/Contact/index.tsx
+++ src/components/AboutAuthor/Contact/index.tsx
@@ -9,21 +9,24 @@ interface AboutAuthorContact {
 export default function AboutAuthorContact({ authorInfo }: AboutAuthorContact) {
   return (
     <div className={styles.contactAuthor}>
-      <h3>JavaScript Personal Training</h3>
+      <h3>Your JavaScript Coach</h3>
       <div className={styles.description}>
         <p>
-          I know how cumbersome are closures, prototypes, hoisting, <i>this</i>, and other JavaScript concepts. 
+          I know how cumbersome are JavaScript concepts like closures, prototypes, hoisting, <i>this</i>.   
         </p>
-        <p>
-          Book a personal JavaScript training session and I will help you overcome the difficulties.  
-        </p>
+        <p>Let me be your JavaScript coach:</p>
+        <ul>
+          <li>I will help you overcome your difficulties in understanding JavaScript</li>
+          <li>One-to-one, remote, 1 hour sessions</li>
+          <li>Informal, conversational and friendly format</li>
+        </ul>
       </div>
       <div className={styles.links}>
         <a className={styles.icon} href={`mailto:${authorInfo.email}`} title={`Write a message to ${authorInfo.name}`}>
           <img alt="Email address" src="/icons/email.svg" />
         </a>
-        <a className={styles.text} href={`mailto:${authorInfo.email}?subject=Book a JavaScript training session`} title={`Write a message to ${authorInfo.name}`}>
-          Book a Training Session
+        <a className={styles.text} href={`mailto:${authorInfo.email}?subject=Book a JavaScript coaching session`} title={`Write a message to ${authorInfo.name}`}>
+          Book a Coaching Session
         </a>
       </div>
     </div>
