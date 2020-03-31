11cb586ccae2f0b7442e070fc717b583391b2848
Dmitri Pavlutin <dpavlutin@gmail.com>
2020-03-31 15:04:33 +0300

    Update about author

--- gatsby/config/site-metadata.js
+++ gatsby/config/site-metadata.js
@@ -14,7 +14,7 @@ module.exports = {
   authorInfo: {
     name: 'Dmitri Pavlutin',
     description:
-      'I\'m a passionate software developer, tech writer and coach. My daily routine consists of (but not limited to) drinking coffee, coding, writing, coaching, fighting boredom 😉.',
+      'I\'m a passionate software developer, tech writer and coach. My daily routine consists of (but not limited to) drinking coffee, coding, writing, coaching, overcoming boredom 😉.',
     email: 'dmitripavlutin@gmail.com',
     jobTitle: 'Software Developer',
     profiles: {
