cf0800903fcd54fbea2892a5b32185b60e29e552
Dmitri Pavlutin <dpavlutin@gmail.com>
2020-10-27 14:13:28 +0200

    Comments count from Github issues

--- src/components/Comments/Count/index.tsx
+++ src/components/Comments/Count/index.tsx
@@ -1,3 +1,26 @@
-export default function CommentsCount(): string {
-  return 'Start discussion';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+interface CommentsCountProps {
+  postUrl: string;
+}
+
+export default function CommentsCount({ postUrl }: CommentsCountProps): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const loadCommentsCount = async () => {
+      const q = `${postUrl}+in:title+is:issue+repo:panzerdp/dmitripavlutin.com-comments`;
+      const respone = await fetch(`https://api.github.com/search/issues?q=${encodeURIComponent(q)}&per_page=1`);
+      const { items = [] } = await respone.json();
+      if (items.length > 0) {
+        const issue = items[0];
+        if (issue.title === postUrl) {
+          setCount(issue.comments);
+        }
+      }
+    };
+    loadCommentsCount();
+  }, []);
+  return <>{ count === 0 ? 'Start discussion' : `${count} Comments` }</>;
 }
