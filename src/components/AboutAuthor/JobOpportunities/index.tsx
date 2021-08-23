57c71e09400a59d62d09c344d5356256699a2d19
Dmitri Pavlutin <dpavlutin@gmail.com>
2021-08-23 20:02:29 +0300

    Update opportunity test

--- src/components/AboutAuthor/JobOpportunities/index.tsx
+++ src/components/AboutAuthor/JobOpportunities/index.tsx
@@ -4,7 +4,7 @@ export default function JobOpportunities() {
   return (
     <div className={styles.jobOpportunities}>
       <h3>
-        Would you like me in your team?
+        Here I am
       </h3>
       <div className={styles.description}>
         <p>
@@ -14,8 +14,8 @@ export default function JobOpportunities() {
         If you&apos;re a recruiter, you&apos;re welcome to <a href="mailto:dmitripavlutin@gmail.com">contact me</a> with an interesting Frontend job position.
         </p>
         <p>
-          If you&apos;re a developer enjoying my posts, you&apos;re welcome to <a href="mailto:dmitripavlutin@gmail.com">contact me</a> to refer me to a Frontend job position in your company.
-        </p> 
+          If you&apos;re a developer enjoying my posts and you want to refer me, you&apos;re also welcome to <a href="mailto:dmitripavlutin@gmail.com">contact me</a>.
+        </p>
       </div>
     </div>
   );
