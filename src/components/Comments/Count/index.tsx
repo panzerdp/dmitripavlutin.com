5eb28c0682900f032b2aea0faaf8041f9472fc86
Dmitri Pavlutin <dpavlutin@gmail.com>
2021-08-24 14:14:45 +0300

    Fix comments count

--- src/components/Comments/Count/index.tsx
+++ src/components/Comments/Count/index.tsx
@@ -10,11 +10,10 @@ export default function CommentsCount({ postUrl, githubCommentsRepository }: Com
   useEffect(() => {
     const loadCommentsCount = async () => {
       const q = `${postUrl}+in:title+is:issue+repo:${githubCommentsRepository}`;
-      const respone = await fetch(`https://api.github.com/search/issues?q=${q}&per_page=1`);
+      const respone = await fetch(`https://api.github.com/search/issues?q=${q}&per_page=5`);
       const { items = [] } = await respone.json();
       for (const item of items) {
         const cleanTitle = item.title.replace(/\//g, '');
-        console.log(cleanTitle, postUrl);
         if (cleanTitle === postUrl) {
           setCount(item.comments);
           break;
