349d646657af6853c449185660fe06dd7036e5f8
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-08-08 09:15:05 +0200

    Add display swap and preload gfonts

--- src/components/Layout/Meta/Tags/index.tsx
+++ src/components/Layout/Meta/Tags/index.tsx
@@ -5,16 +5,14 @@ interface LayoutMetaTagsProps {
 }
 
 export default function LayoutMetaTags({ siteInfo }: LayoutMetaTagsProps): JSX.Element {
+  const fontsUrl = '//fonts.googleapis.com/css?family=Open+Sans:700|EB+Garamond:400,400i,600,700Roboto+Mono:400&display=swap'
   return (
     <Helmet>
       <link rel="preconnect" href="//fonts.gstatic.com/" crossOrigin="" />
       <title>{siteInfo.metaTitle}</title>
       <meta name="description" content={siteInfo.metaDescription} />
-      <link
-        href="//fonts.googleapis.com/css?family=Open+Sans:700|EB+Garamond:400,400i,600,700|Roboto+Mono:400"
-        rel="stylesheet"
-        type="text/css"
-      />
+      <link rel="preload" href={fontsUrl} as="style" />
+      <link href={fontsUrl} rel="stylesheet" type="text/css" />
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
       <meta name="HandheldFriendly" content="True" />
       <link rel="shortcut icon" href="/favicon.ico" />
