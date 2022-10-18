07fd6089668545b3b78f93d7f3d32d17469476ff
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-10-18 09:18:11 +0200

    Comments loading placeholder dark mode

--- src/components/Skeleton/index.tsx
+++
@@ -1,7 +0,0 @@
-import classNames from 'classnames';
-
-import * as styles from './index.module.scss';
-
-export function SkeletonAnimation (): JSX.Element {
-  return <div className={classNames(styles.skeleton, styles.fill)} />;
-}
