d2afc4972fc29c6013db60e94c7aeeb8787df672
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-03-04 17:18:20 +0100

    chore: move content/posts to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,8 +89,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content`,
-        name: 'content',
+        path: `${__dirname}/posts`,
+        name: 'posts',
       },
     },
     {
