2c7c83fdc5c28696ab65ece172bd7db515e6cc3b
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-03-25 11:14:17 +0100

    chore: make generate cover images at /:slug/cover/ paths

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require('path')
 
-const createExcerptsList = require('./gatsby/node/excerpts-list');
-const createPost = require('./gatsby/node/post');
-const createPlainListByTag = require('./gatsby/node/plain-list-by-tag');
- 
- 
+const createExcerptsList = require('./gatsby/node/excerpts-list')
+const createPost = require('./gatsby/node/post')
+const createPlainListByTag = require('./gatsby/node/plain-list-by-tag')
+
+
 const query = `
 # PostThumbnail fragment should be everywhere exactly the same to guarantee the same images generation
 fragment PostThumbnail on MdxFrontmatter {
   thumbnail {
     childImageSharp {
-      gatsbyImageData(aspectRatio: 1.8, quality: 60, width: 708, formats: [WEBP], layout: CONSTRAINED, placeholder: NONE)
+      gatsbyImageData(aspectRatio: 1.8, quality: 60, width: 708, formats: [WEBP, PNG], layout: CONSTRAINED, placeholder: NONE)
     }
   }
 }
@@ -55,46 +55,46 @@ query CreatePages {
       }
     }
   }
-}`;
+}`
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const result = await graphql(query);
+  const { createPage } = actions
+  const result = await graphql(query)
   if (result.errors) {
     // eslint-disable-next-line no-console
-    console.log(result.errors);
-    throw result.errors;
+    console.log(result.errors)
+    throw result.errors
   }
   // Create blog posts pages.
-  const edges = result.data.allMdx.edges;
+  const edges = result.data.allMdx.edges
 
-  const popularPostsByCategory = result.data.site.siteMetadata.featured.popularPostsByCategory;
-  const popularPostsSlugs = popularPostsByCategory.reduce((acc, postsByCategory) => [...acc, ...postsByCategory.slugs], []);
+  const popularPostsByCategory = result.data.site.siteMetadata.featured.popularPostsByCategory
+  const popularPostsSlugs = popularPostsByCategory.reduce((acc, postsByCategory) => [...acc, ...postsByCategory.slugs], [])
 
-  createExcerptsList(createPage, edges, popularPostsSlugs);
-  createPost({ createPage, edges, popularPostsSlugs }, actions);
-  createPlainListByTag(createPage, edges);
-  return result;
-};
+  createExcerptsList(createPage, edges, popularPostsSlugs)
+  createPost({ createPage, edges, popularPostsSlugs }, actions)
+  createPlainListByTag(createPage, edges)
+  return result
+}
 
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig  }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
-  });
+  })
 
   // Remove CSS ordering logs
   if (stage === 'build-javascript' || stage === 'develop') {
-    const config = getConfig();
+    const config = getConfig()
     const miniCssExtractPlugin = config.plugins.find(
       plugin => {
-        return plugin.constructor.name === 'MiniCssExtractPlugin';
+        return plugin.constructor.name === 'MiniCssExtractPlugin'
       }
-    );
+    )
     if (miniCssExtractPlugin) {
-      miniCssExtractPlugin.options.ignoreOrder = true;
+      miniCssExtractPlugin.options.ignoreOrder = true
     }
-    actions.replaceWebpackConfig(config);
+    actions.replaceWebpackConfig(config)
   }
-};
+}
