64ada772e379e77fe8e762f3aaf56d8c334ca296
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-04-10 13:59:30 +0200

    content: update newsletter page

--- src/components/Pages/Newsletter/Template/index.tsx
+++ src/components/Pages/Newsletter/Template/index.tsx
@@ -2,14 +2,17 @@ import * as styles from './index.module.scss'
 import Layout from 'components/Layout/Fetch'
 import { SubscriptionRegion } from 'components/Subscription/Region'
 import MetaTags from 'components/Pages/Newsletter/Meta/Tags'
+import { NewsletterRightSidebar } from '../RightSidebar'
 
 export default function NewsletterTemplate() {
   return (
-    <Layout>
+    <Layout rightSidebar={<NewsletterRightSidebar />}>
       <MetaTags />
       <div className={styles.newsletter}>
         <h1>Newsletter</h1>
-        <SubscriptionRegion />
+        <div className={styles.subscription}>
+          <SubscriptionRegion />
+        </div>
       </div>
     </Layout>
   )
