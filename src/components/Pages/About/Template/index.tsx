cc59e4ffb9bb8a1a942e7778855edbe0fb95fe36
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-05-18 15:36:27 +0200

    content: remove picture from about me

--- src/components/Pages/About/Template/index.tsx
+++ src/components/Pages/About/Template/index.tsx
@@ -1,29 +1,15 @@
 import Layout from 'components/Layout/Fetch'
 import AboutMetaTags from 'components/Pages/About/Meta/Tags'
 import AboutMetaStructuredData from 'components/Pages/About/Meta/StructuredData'
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
 
 export default function AboutTemplate() {
-  const data = useStaticQuery(
-    graphql`
-      query AboutPictureQuery {
-        aboutPicture: file(relativePath: { eq: "dmitri-pavlutin-seine-4.jpg" }) {
-          childImageSharp {
-            gatsbyImageData(width: 600, quality: 60, layout: CONSTRAINED, formats: [AUTO, WEBP])
-          }
-        }
-      }
-    `
-  )
-
   return (
     <Layout>
       <AboutMetaTags />
       <AboutMetaStructuredData />
       <h1>About me</h1>
 
-      <p>My name is Dmitri Pavlutin.I&apos;m a Senior Frontend Developer with 10+ years of experience, having a Master&apos;s Degree in Computer Science.</p>
+      <p>My name is Dmitri Pavlutin. I&apos;m a Senior Frontend Developer with 10+ years of experience, having a Master&apos;s Degree in Computer Science.</p>
 
       <p>Currently, I live in the sunny city of Barcelona (Spain) with my wife and my cat.</p>
 
@@ -33,7 +19,7 @@ export default function AboutTemplate() {
 
       <p>On this blog, I publish regularly posts about Frontend development, mainly about JavaScript, TypeScript, Vue.js, and React.js. Writing helps me sharpen my writing skills and learn a lot along the way.</p>
 
-      <GatsbyImage alt="Dmitri Pavlutin photo" image={data.aboutPicture.childImageSharp.gatsbyImageData} />
+      <p>For any questions or concerns regarding this blog, please send me an email at <a href="mailto:dmitripavlutin@gmail.com">dmitripavlutin@gmail.com</a>.</p>
     </Layout>
   )
 }
