5167fbe4bc5d0fd752bef733ba24e58042cfe1a6
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-05-18 14:53:10 +0200

    content: update about me page, reduce footer links font size

--- src/components/Pages/About/Template/index.tsx
+++ src/components/Pages/About/Template/index.tsx
@@ -23,9 +23,15 @@ export default function AboutTemplate() {
       <AboutMetaStructuredData />
       <h1>About me</h1>
 
-      <p>My name is Dmitri Pavlutin.</p>
+      <p>My name is Dmitri Pavlutin.I&apos;m a Senior Frontend Developer with 10+ years of experience, having a Master&apos;s Degree in Computer Science.</p>
 
-      <p>I write about frontend development, mainly about JavaScript, TypeScript, and React.js. Writing helps me sharpen my written communication skills and stay in touch with software technology progress.</p>
+      <p>Currently, I live in the sunny city of Barcelona (Spain) with my wife and my cat.</p>
+
+      <p>My passion is web applications development: programming, design (UI and UX), automated testing, and good application architecture. I&apos;m skilled at JavaScript, TypeScript, HTML, CSS, including React.js and Vue.js frameworks.</p>
+
+      <p>I have good communication skills (being fluent in written and verbal English) and interpersonal skills. I strive to be a humble team player and be approachable to receive and give feedback.</p>
+
+      <p>On this blog, I publish regularly posts about Frontend development, mainly about JavaScript, TypeScript, Vue.js, and React.js. Writing helps me sharpen my writing skills and learn a lot along the way.</p>
 
       <GatsbyImage alt="Dmitri Pavlutin photo" image={data.aboutPicture.childImageSharp.gatsbyImageData} />
     </Layout>
