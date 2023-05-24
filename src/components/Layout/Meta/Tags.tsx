369ef722406f7e9d916d974f126666b4e9341aea
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-05-24 19:26:21 +0200

    feat: include bsa script

--- src/components/Layout/Meta/Tags.tsx
+++ src/components/Layout/Meta/Tags.tsx
@@ -6,6 +6,9 @@ interface LayoutMetaTagsProps {
 
 export default function LayoutMetaTags({ siteInfo }: LayoutMetaTagsProps): JSX.Element {
   const isProduction = process.env.NODE_ENV === 'production'
+
+  const timestampAsVersion = Date.now() - Date.now() % 600000
+
   return (
     <Helmet>
       <title>{siteInfo.metaTitle}</title>
@@ -17,7 +20,7 @@ export default function LayoutMetaTags({ siteInfo }: LayoutMetaTagsProps): JSX.E
       <meta name="robots" content="index, follow" />
       <html lang="en" />
       <meta name="google-site-verification" content="A3lH-k4h-4bEnJ4lt6BsPuTh5iUck5ElEV5xeyvkCxo" />
-      {isProduction && <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5428678507937314" crossOrigin="anonymous" /> }
+      {isProduction && <script async src={'https://cdn4.buysellads.net/pub/dmitripavlutin.js?'+ timestampAsVersion} crossOrigin="anonymous" /> }
     </Helmet>
   )
 }
