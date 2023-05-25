5be0a625609884836b115ae0ad60125f9170d9db
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-05-25 12:33:11 +0200

    fix: refresh footer ad

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -8,4 +8,14 @@ exports.onInitialClientRender = () => {
   const timestampAsVersion = Date.now() - Date.now() % 600000
   baseOptimize.src = 'https://cdn4.buysellads.net/pub/dmitripavlutin.js?' + timestampAsVersion
   document.head.appendChild(baseOptimize)
+
+  var optimizeFixedFooterCheck = setInterval(function() {
+    if (document.querySelector('.bsa_fixed-leaderboard')) {
+      clearInterval(optimizeFixedFooterCheck)
+      window.optimize = window.optimize || { queue: [] }
+      window.optimize.queue.push(function() {
+        window.optimize.push('bsa-zone_1684483359071-0_123456')
+      })
+    }
+  }, 300) // check every 300ms
 }
