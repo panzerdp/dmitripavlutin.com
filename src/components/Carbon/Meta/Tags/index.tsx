b6b69019cee79f0aa84562801ae09c2839b7ad12
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-06-05 19:11:52 +0200

    chore: preload as script

--- src/components/Carbon/Meta/Tags/index.tsx
+++ src/components/Carbon/Meta/Tags/index.tsx
@@ -10,7 +10,7 @@ export default function LayoutCarbonAdsMetaTags({
   if (isEnabled && isProductionMode) {
     return (
       <Helmet>
-        <link rel="preload" href={scriptSrc} />
+        <link rel="preload" href={scriptSrc} as="script" />
         <link rel="preconnect" href="//cdn4.buysellads.net" />
         <link rel="preconnect" href="//srv.carbonads.net" />
       </Helmet>
