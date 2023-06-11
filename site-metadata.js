45838bde8b3b2d1fa694021d22b78816785bcdca
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-06-11 10:17:09 +0200

    feat: javascript vueschool course

--- site-metadata.js
+++ site-metadata.js
@@ -61,6 +61,19 @@ module.exports = {
     }]
   },
   affiliates: [{
+    tags: ['javascript'],
+    text: `
+        <p>
+          Before I go on, let me recommend something to you.
+        </p>
+        <p>
+          The path to becoming good at JavaScript isn't easy... but fortunately with a good course you can shortcut.
+        </p>
+        <p>
+        Take <a href="__URL__" target="_blank" rel="noopener">"Modern JavaScript: ES6 and beyond!"</a> course by Vueschool to become proficient in JavaScript in just a few weeks. Use the coupon code DMITRI and get your 20% discount!
+        </p>`,
+    url: 'https://vueschool.io/courses/modern-javascript-es6-and-beyond?friend=dmitripavlutin'
+  }, {
     tags: ['vue'],
     text: `
       <p>
