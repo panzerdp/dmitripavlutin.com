dc106f137510546ae249e5898e7517492b11dad4
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-06-13 10:05:13 +0200

    Revert "feat: move carbon ad into the paragraph"
    This reverts commit 71cb67f878ebf11d14c9ab96aa6a6e0a44f0b4ba.

--- src/components/Pages/Post/Sidebar/Left/index.tsx
+++ src/components/Pages/Post/Sidebar/Left/index.tsx
@@ -1,6 +1,9 @@
+import Media from 'react-media'
+
 import * as styles from './index.module.scss'
 
 import ShareGroupVertical from 'components/Pages/Post/Share/Group/Vertical'
+import { CarbonAdsSection } from 'features/carbonAds'
 import { PostPlain } from 'typings/post'
 
 interface PostLeftSidebarProps {
@@ -11,6 +14,9 @@ interface PostLeftSidebarProps {
 export default function PostLeftSidebar({ post, showShareButtons }: PostLeftSidebarProps) {
   return (
     <div className={styles.leftSidebar}>
+      <Media query="(min-width: 1251px)" defaultMatches={false}>
+        <CarbonAdsSection />
+      </Media>
       <ShareGroupVertical post={post} show={showShareButtons} />
     </div>
   )
